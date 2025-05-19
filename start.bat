a3025a88dfe5df787704430fdc889d846d360e23
xhmcc <2670841984@qq.com>
2025-05-19 11:02:05 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo.
-echo usage: python icpsearch.py -f input.xlsx -o output.xlsx
-echo usage: python icpsearch.py -f input.xlsx -o output.xlsx -proxy http://127.0.0.1:8080
+echo usage: python icpsearch_aqc.py -f input.xlsx -o output.xlsx
+echo usage: python icpsearch_icp.py -f input.xlsx -o output.xlsx -proxy http://127.0.0.1:8080
 echo.
 call cmd
