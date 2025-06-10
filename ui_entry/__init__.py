a09929618209f1c75bcf080bd12134b9118a18df
xhmcc <2670841984@qq.com>
2025-06-10 14:50:08 +0800

    Add files via upload

--- ui_entry/__init__.py
+++ ui_entry/__init__.py
@@ -1,1 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui_mainwindow import MainWindow
 
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main() 
